5c4ee907d2ab5da327cd131ed7db259c83a3945f
sumitbishnoi00 <sumitbishnoi369@gmail.com>
2026-07-22 14:49:39 +0530

    add section

--- src/components/common/Icons.jsx
+++ src/components/common/Icons.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { jsx } from "react/jsx-runtime";
 
 const Icon = ({ icon, className, pathName }) => {
-  const icons = {
-    
-  };
+    const icons = {
+        check: (
+            <svg className={className} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_2437_493)">
+                    <path className={pathName} d="M12.0001 21.5999L6.40011 15.9999L4.53345 17.8666L12.0001 25.3332L28.0001 9.33322L26.1334 7.46655L12.0001 21.5999Z" fill="#3B5998" />
+                </g>
+                <defs>
+                    <clipPath id="clip0_2437_493">
+                        <rect width="32" height="32" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
 
-  return icons[icon];
+        ),
+    };
+
+    return icons[icon];
 };
 
 export default Icon;
---
+++ src/utils/helper.js
@@ -0,0 +1,40 @@
+export const CRM_DATA = [
+
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Euis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+
+];
+
+
+export const MARKETING_DATA = [
+
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Amet cursus sit amet dictum sit amet justo donec enim diam. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae auctor.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Euis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+
+];
+
+export const ANALYTICS_DATA = [
+
+    "Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Eu augue ut lectus arcu bibendum at varius vel pharetra. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Euis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+
+];
+
+export const SECURITY_DATA = [
+
+    "Integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum. Quisque non tellus orci ac auctor augue mauris augue neque. Pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes.",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    "Euis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+
+];
